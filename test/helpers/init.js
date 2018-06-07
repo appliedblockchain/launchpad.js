84fd31e274784789c8207eb73822fc37b84e215e
Jamie Allen <jamie@appliedblockchain.com>
2018-06-07 16:32:03 +0100

    overrode logger to clean up test output

--- test/helpers/init.js
+++ test/helpers/init.js
@@ -1,10 +1,11 @@
+const notALogger = require('@appliedblockchain/not-a-logger')
 const { join } = require('path')
 
-const init = ({ gas = 50000000 }) => {
+const init = ({ gas = 50000000, logger = notALogger } = {}) => {
   const { web3, accounts } = require('@appliedblockchain/cobalt/web3')({
     root: join(__dirname, '..', '..', 'contracts'),
     accounts: 10,
-    logger: console
+    logger
   })
 
   const addresses = accounts.map(account => account.address)
