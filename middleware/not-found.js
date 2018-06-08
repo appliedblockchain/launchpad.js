704afc41a4cde1f4a6191b6d8d4fcd0c97e19000
Jamie Allen <jamie@appliedblockchain.com>
2018-06-08 16:44:57 +0100

    added koa-respond

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,11 +1,11 @@
 'use strict'
 
-module.exports = async (ctx) => {
-  console.log('CTX', ctx)
-  const { request } = ctx
+module.exports = async ({ request, notFound }) => {
   const { method, path } = request
 
   const message = `${method} ${path}`
-  console.log('Not found', ctx.notFound)
-  ctx.body = `No endpoint matched your request: ${message}`
+
+  notFound({
+    message: `No endpoint matched your request: ${message}`
+  })
 }
