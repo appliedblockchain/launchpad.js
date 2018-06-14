bc7111b7dc0280cf1166964dcf67980af8065249
Jamie Allen <jamie@appliedblockchain.com>
2018-06-14 14:43:23 +0100

    added getLatestWriter function to Store contract

--- test/store.test.js
+++ test/store.test.js
@@ -5,6 +5,7 @@ const { from, gas, web3 } = init()
 web3.require('Store.sol')
 
 describe('Store', () => {
+  const data = 'someData0x12345'
   let Store
 
   afterAll(async () => {
@@ -19,11 +20,18 @@ describe('Store', () => {
   })
 
   it('sets a value', async () => {
-    const data = 'someData0x12345'
     await Store.methods.set(data).send({ from, gas })
 
     const result = await Store.methods.get().call()
 
     expect(result).toEqual(data)
+  })
+
+  it('stored the latest writer to the contract', async () => {
+    await Store.methods.set(data).send({ from, gas })
+
+    const result = await Store.methods.getLatestWriter().call()
+
+    expect(result).toEqual(from)
   })
 })
