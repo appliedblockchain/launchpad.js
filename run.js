ad95f8051dfeb2d6618e8a71e13d2a94dd330f4c
Jamie Allen <jamie@appliedblockchain.com>
2018-06-19 10:52:32 +0100

    added basic router health tests

---
+++ run.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const runApp = require('./lib/app')
+const logger = require('./lib/logger')
+const readFileContents = require('./readFileContents')
+
+const getContractAddress = (text) => {
+  const results = text.match(/0x\S+/)
+
+  return !results ? '' : results[0]
+}
+
+readFileContents('./contract-address.txt', async (err, contractAddressFile) => {
+  if (err) {
+    logger.error('Could not retrieve ./contract-address.txt file', err)
+  }
+
+  const contractAddress = getContractAddress(contractAddressFile)
+
+  if (contractAddress.length) {
+    await runApp(contractAddress)
+  } else {
+    logger.error('There was no contract address present in ./contract-address.txt', err)
+  }
+})
