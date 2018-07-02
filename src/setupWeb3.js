5b8744a29d68c792fed16c8ffac12e3a74c2e205
Jamie Allen <jamie@appliedblockchain.com>
2018-07-02 12:13:14 +0100

    added ETHEREUM_CLIENT_ADDRESS as a environment variable

--- src/setupWeb3.js
+++ src/setupWeb3.js
@@ -1,4 +1,6 @@
 const Web3 = require('web3')
+const config = require('config')
+const ETHEREUM_CLIENT_ADDRESS = config.get('ETHEREUM_CLIENT_ADDRESS')
 
 const getFromAddress = async (web3) => {
   const accounts = await web3.eth.getAccounts()
@@ -10,7 +12,7 @@ const getFromAddress = async (web3) => {
 const setupWeb3 = async ({
   abi,
   contractAddress,
-  ethereumClientAddress = 'http://localhost:8545'
+  ethereumClientAddress = ETHEREUM_CLIENT_ADDRESS
 }) => {
   const web3 = new Web3(ethereumClientAddress)
 
