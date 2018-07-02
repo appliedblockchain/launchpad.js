93c995ca64b2646ae4487799aa721cd7ed7ac8e3
Jamie Allen <jamie@appliedblockchain.com>
2018-07-02 15:15:52 +0100

    added git tags into healthcheck

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ ARG NPM_TOKEN
 RUN echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
 
 ENV GIT_COMMIT_SHA ${GIT_COMMIT_SHA}
+ENV GIT_TAG ${GIT_TAG}
 
 COPY package.json /app/package.json
 RUN npm i --only=production
--- src/healthcheck.js
+++ src/healthcheck.js
@@ -1,9 +1,13 @@
 const { healthcheck: abHealthcheck } = require('@appliedblockchain/koa-healthcheck')
+const commitSha = process.env.GIT_COMMIT_SHA
+const gitTag = process.env.GIT_TAG
 
 const healthcheck = (contractAddress) => {
   return abHealthcheck({
     custom: {
-      storeContractAddress: contractAddress
+      storeContractAddress: contractAddress,
+      commit: commitSha || 'No commit was passed into this build',
+      tag: gitTag || 'There was no tag associated with this build'
     }
   })
 }
