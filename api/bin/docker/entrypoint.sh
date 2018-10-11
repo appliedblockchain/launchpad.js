5c871d9842c02e63a6223e0ea7a548e7faaef6c3
Alaan Rahim <alaan@appliedblockchain.com>
2018-10-11 11:55:28 +0100

    Use @appliedblockchain/consul package for API deployment

--- api/bin/docker/entrypoint.sh
+++ api/bin/docker/entrypoint.sh
@@ -2,17 +2,16 @@
 
 set -ex
 
-cd /contracts/consul
-
 CONTRACT_KEY="mantle-development/contract-addresses"
 LEADER_KEY="mantle-development/leader"
 
 # All nodes will attempt to acquire a lock. The last node to acquire
 # the lock is elected as leader
-node consul-utility set-key-value $LEADER_KEY "$HOSTNAME"
-sleep 2
-LEADER=$(node consul-utility get-key-value $LEADER_KEY)
-CONTRACT_ADDRESSES_EXIST=$(node consul-utility check-key-exists $CONTRACT_KEY)
+npx @appliedblockchain/consul set-kv $LEADER_KEY "$HOSTNAME"
+
+sleep 5
+LEADER=$(npx @appliedblockchain/consul get-kv $LEADER_KEY)
+CONTRACT_ADDRESSES_EXIST=$(npx @appliedblockchain/consul check-key-exists $CONTRACT_KEY)
 
 # Only re-deploy if no contract address exists, otherwise the address will already be available in consul
 if [ "$CONTRACT_ADDRESSES_EXIST" = "false" ]; then
@@ -21,16 +20,16 @@ if [ "$CONTRACT_ADDRESSES_EXIST" = "false" ]; then
       CONTRACT_ADDRESSES=$(cat /api/contracts/exportAddresses.sh)
 
       sleep 5
-      node consul-utility set-key-value $CONTRACT_KEY "$CONTRACT_ADDRESSES"
+      npx @appliedblockchain/consul set-kv $CONTRACT_KEY "$CONTRACT_ADDRESSES"
   else
-      while [ $(node consul-utility check-key-exists $CONTRACT_KEY) = "false" ]
+      while [ $(npx @appliedblockchain/consul check-key-exists $CONTRACT_KEY) = "false" ]
       do
           sleep 5
       done
   fi
 fi
 
-node consul-utility get-key-value $CONTRACT_KEY > /api/contracts/exportAddresses.sh
+npx @appliedblockchain/consul get-kv $CONTRACT_KEY > /api/contracts/exportAddresses.sh
 cat /api/contracts/exportAddresses.sh
 cd /api
 npm start
--- contracts/consul/config/consul-config.js
+++
@@ -1,8 +0,0 @@
-// TODO: extract in a config file
-const HOST = 'discovery-eu-west-1-dev.aws.appb.ch'
-const CONSUL_TOKEN = '48593f6f-8152-7e9c-8485-3f6bd4b65619'
-
-module.exports = {
-  HOST,
-  CONSUL_TOKEN
-}
