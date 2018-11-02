e3b2c05442e7b0fd60771e5cf9e3fc269e76753b
Rémi Bouchar <remi.bouchar@gmail.com>
2018-11-02 15:54:49 +0000

    Created a docker dev environment

---
+++ docker-dev/compose.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")" || exit 1
+
+configArgs=""
+configPathList=(docker-compose-dev.yml)
+npmrcPath="$HOME/.npmrc"
+
+for path in "${configPathList[@]}"; do
+  configArgs="$configArgs -f $path"
+done
+
+show_usage() {
+  echo "\
+Usage:
+  $0 -h
+  $0 [-t NPM_TOKEN] ARGS...
+
+Flags:
+  -h            : Show this message and exit
+  -t NPM_TOKEN  : A valid token to access the Applied Blockchain private npm repos. If omitted then the current token will be extracted from '$npmrcPath' and used.
+
+Runs 'docker-compose$configArgs ARGS...'
+" | fold -s
+}
+
+while getopts "ht:" opt; do
+  case $opt in
+    h)
+      show_usage
+      exit
+      ;;
+    t)
+      npmToken="$OPTARG"
+      ;;
+    \?)
+      show_usage
+      exit 2
+      ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+
+if [[ -z $npmToken ]]; then
+  if [[ ! -f $npmrcPath ]]; then
+    echo "$npmPath file not found, so NPM_TOKEN argument required"
+    exit 2
+  else npmToken="$(cat "$npmrcPath" | grep _authToken |sed 's/.*=\(.*$\)/\1/')"
+  fi
+fi
+
+
+# echo 'installing contracts in app folders(shared_modules)'
+npm --prefix ../contracts run install-contracts
+
+echo 'Loading contracts addresses in env'
+source ../api/contracts/exportAddresses.sh
+
+env COMPOSE_PROJECT_NAME="base-app-mantle" NPM_TOKEN="$npmToken" docker-compose $configArgs $@
