96fa877b59d4c4cedafa73925fc45217365307fd
Alaan Rahim <alaan@appliedblockchain.com>
2018-11-06 14:32:07 +0000

    save backup files to S3

--- stack/docker/cron-job.js
+++ stack/docker/cron-job.js
@@ -1,8 +1,39 @@
 const cron = require('node-cron')
+const fs = require('fs')
+const AWS = require('aws-sdk')
 const { exec } = require('child_process')
 
+const s3 = new AWS.S3()
+
 cron.schedule('* * * * *', () => {
-  console.log('RUNNING...')
+  console.log('RUNNING PARITY BACKUP TASK...')
+
+  const backupName = createFilename('base-app-mantle-parity')
+  const command = `sh cron-job.sh ${backupName}`
+
+  const execution = exec(command)
+  execution.stdout.on('data', (data) => {
+    // Archiving has been completed - we can now upload to S3
+    if (data.trim() === 'Archive complete...') {
+      const body = fs.readFileSync(`./${backupName}`)
+      const params = {
+        Bucket: 'ab-parity-backups',
+        Body: body,
+        Key: backupName
+      }
+
+      s3.upload(params, (err, data) => {
+        if (err) {
+          console.log(`S3 upload error: ${err}`)
+        } else {
+          console.log(`S3 upload success: "${data.key}" uploaded to "${data.Bucket}" bucket`)
+        }
+      })
+    }
+  })
+})
+
+function createFilename(prefix) {
   const date = new Date()
 
   let day = date.getDate()
@@ -10,15 +41,8 @@ cron.schedule('* * * * *', () => {
 
   let month = date.getMonth() + 1
   month = month < 10 ? `0${month}` : `${month}`
-
+ 
   const year = `${date.getFullYear()}`
 
-  const backupName = `base-app-mantle-parity-${day}-${month}-${year}.tar.gz`
-  const command = `sh cron-job.sh ${backupName}`
-
-  exec(command, (err, stdout, stderr) => {
-    console.log('err', err)
-    console.log('STDOUT', stdout)
-    console.log('STDERR', stderr)
-  })
-})
+  return `${prefix}-${day}-${month}-${year}.tar.gz`
+}
--- stack/docker/cron-job.sh
+++ stack/docker/cron-job.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 BACKUP_NAME="$1"
 tar -czvf "$BACKUP_NAME" /parity/data/chains/parity
+
+# echo "hello" > ./test.txt
+echo "Archive complete..."
