88c19d5b92f853d5c190d6d0b7e0d423602634ba
Rémi Bouchar <remi.bouchar@gmail.com>
2018-11-09 13:39:16 +0000

    avoid sending the mnemonic to the server

--- api/src/api/notes/index.js
+++ api/src/api/notes/index.js
@@ -1,7 +1,8 @@
-const getNote = require('./getNote')
-const addNote = require('./addNote')
-const getNotesCount = require('./getNotesCount')
-const getNotes = require('./getNotes')
+const getNote = require('./getNote.js')
+const addNote = require('./addNote.js')
+const getNotesContract = require('./getNotesContract.js')
+const getNotesCount = require('./getNotesCount.js')
+const getNotes = require('./getNotes.js')
 const searchNotes = require('./searchNotes.js')
 
-module.exports = [ searchNotes, addNote, getNotesCount, getNote, getNotes ]
+module.exports = [ searchNotes, addNote, getNotesCount, getNote, getNotes, getNotesContract ]
