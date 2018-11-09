eb0972aa2668cff4c8586276edf08e0cd0a8c602
Michael <michael@appliedblockchain.com>
2018-11-09 14:17:22 +0000

    Refactoring the routing system

--- react/src/constants.js
+++ react/src/constants.js
@@ -1,1 +1,13 @@
 export const APP_PREFIX = '@react-base-app-mantle'
+
+export const ROUTE_URL = {
+  startScreen: '/',
+  generateMnemonic: '/generate-mnemonic',
+  loadMnemonic: '/load-mnemonic',
+  notes: '/notes'
+}
+
+export default {
+  ROUTE_URL,
+  APP_PREFIX
+}
--- react/src/containers/App/App.js
+++ react/src/containers/App/App.js
@@ -1,5 +1,5 @@
-import React, { Fragment } from 'react'
-/* import PropTypes from 'prop-types' */
+import React, { Fragment, Component } from 'react'
+import PropTypes from 'prop-types'
 import Page404 from 'containers/Page404'
 import { PersistGate } from 'redux-persist/es/integration/react'
 import { ConnectedRouter } from 'react-router-redux'
@@ -8,9 +8,7 @@ import { Switch, Redirect } from 'react-router-dom'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import config from 'store'
-import PropTypes from 'prop-types'
-// app modules
-import { ROUTE_URL } from 'containers/constants.js'
+import { ROUTE_URL } from 'constants.js'
 import StartScreen from 'containers/StartScreen'
 import GenerateMnemonic from 'containers/GenerateMnemonic'
 import LoadMnemonic from 'containers/LoadMnemonic'
@@ -22,11 +20,21 @@ import HeaderBack from 'components/HeaderBack'
 
 const { history, persistor } = config
 
+const renderNotes = () => (
+  <Fragment>
+    <UserInfo />
+    <NotesAdd />
+    <NotesSearch />
+    <NotesList />
+  </Fragment>
+)
+
+
 const NotAuthenticatedRoutes = () => (
   <Switch>
     <Route exact path={ROUTE_URL.startScreen} component={StartScreen} />
     <Route
-      path={'/generate-mnemonic'}
+      path={ROUTE_URL.generateMnemonic}
       render={props => (
         <Fragment>
           <HeaderBack />
@@ -43,53 +51,38 @@ const NotAuthenticatedRoutes = () => (
         </Fragment>
       )}
     />
-    <Route render={() => <Redirect to={'/generate-mnemonic'} />} />
-
-    <Route component={Page404} />
+    <Route render={() => <Redirect to={ROUTE_URL.generateMnemonic} />} />
+    <Route render={() => <Page404 />} />
   </Switch>
 )
 
 const AuthenticatedRoutes = () => (
   <Switch>
-    <Route
-      path={'/notes/new'}
-      render={() => (
-        <Fragment>
-          <UserInfo />
-          <NotesAdd />
-        </Fragment>
-      )}
-    />
-    <Route
-      path={'/notes'}
-      render={() => (
-        <Fragment>
-          <UserInfo />
-          <NotesAdd />
-          <NotesSearch />
-          <NotesList />
-        </Fragment>
-      )}
-    />
-    <Route render={() => <Redirect to={'/notes'} />} />
-    <Route component={Page404} />
+    <Route path={ROUTE_URL.notes} render={renderNotes()} />
+    <Route render={() => <Redirect to={ROUTE_URL.notes} />} />
+    <Route render={() => <Page404 />} />
   </Switch>
 )
 
-const App = ({ authenticated }) => {
-  return (
-    <PersistGate loading={<div>Loading</div>} persistor={persistor}>
-      <MuiThemeProvider muiTheme={getMuiTheme()}>
-        <ConnectedRouter history={history}>
-          {authenticated ? (
-            <AuthenticatedRoutes />
-          ) : (
-            <NotAuthenticatedRoutes />
-          )}
-        </ConnectedRouter>
-      </MuiThemeProvider>
-    </PersistGate>
-  )
+class App extends Component {
+  renderAvailableRoutes = () => {
+    const { authenticated } = this.props
+    return authenticated ? <AuthenticatedRoutes /> : <NotAuthenticatedRoutes />
+  }
+
+  renderLoading = () => <div>Loading</div>
+
+  render() {
+    return (
+      <PersistGate loading={this.renderLoading()} persistor={persistor}>
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+          <ConnectedRouter history={history}>
+            { this.renderAvailableRoutes() }
+          </ConnectedRouter>
+        </MuiThemeProvider>
+      </PersistGate>
+    )
+  }
 }
 
 App.propTypes = {
