abc3482ae605949ee2bbfdc759d359da9dcc8816
Michael <michael@appliedblockchain.com>
2018-11-13 16:40:46 +0000

    Reworking tests for auth - adding action creator tests

--- react/src/store/auth/index.spec.js
+++ react/src/store/auth/index.spec.js
@@ -1,38 +1,46 @@
-import reducer, { ACTIONS, register } from './index'
+import reducer, { ACTIONS, generateMnemonic, loadMnemonic } from './index'
 
 describe('reducer: store/auth', () => {
-  const { REGISTER, REGISTER_SUCCESS } = ACTIONS
+  const {
+    GENERATE_MNEMONIC, LOAD_MNEMONIC
+  } = ACTIONS
 
   describe('Action Creators', () => {
-    describe('register', () => {
+    describe('generateMnemonic', () => {
+      it('returns the correct type', () => {
+        const produces = { type: GENERATE_MNEMONIC }
+        expect(generateMnemonic()).toEqual(produces)
+      })
+    })
+
+    describe('loadMnemonic', () => {
       it('returns the correct type and payload', () => {
-        expect(register({ expected: true })).toEqual({
-          type: REGISTER,
-          payload: { expected: true }
-        })
+        const given = 'mnemonic'
+        const produces = { type: LOAD_MNEMONIC, payload: given }
+        expect(loadMnemonic(given)).toEqual(produces)
       })
     })
   })
 
-  describe('Reducer', () => {
-    it('has a working default state', () => {
-      expect(reducer('', {})).toEqual('')
-      expect(reducer({ initial: true }, {})).toEqual({ initial: true })
-    })
+  // describe('Reducer', () => {
+  //   it('has a working default state', () => {
+  //     expect(reducer('', {})).toEqual('')
+  //     expect(reducer({ initial: true }, {})).toEqual({ initial: true })
+  //   })
 
-    describe('REGISTER_SUCCESS', () => {
-      it('returns the auth object', () => {
-        expect(
-          reducer(
-            {},
-            { type: REGISTER_SUCCESS, payload: { mnemonic: 'mnemonic' } }
-          )
-        ).toEqual({
-          mnemonic: 'mnemonic',
-          requestPassword: false,
-          derivationError: false
-        })
-      })
-    })
-  })
+  //   describe('REGISTER_SUCCESS', () => {
+  //     it('returns the auth object', () => {
+  //       expect(
+  //         reducer(
+  //           {},
+  //           { type: REGISTER_SUCCESS, payload: { mnemonic: 'mnemonic' } }
+  //         )
+  //       ).toEqual({
+  //         mnemonic: 'mnemonic',
+  //         requestPassword: false,
+  //         derivationError: false
+  //       })
+  //     })
+  //   })
+  // })
 })
