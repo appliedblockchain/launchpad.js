2c50dc1009767d82fead410c3ebc440b020ce184
Sabin Raj Dangol <sawbean.raz@googlemail.com>
2018-11-13 12:35:17 +0000

    Update parity status with err message if error reading last block number

--- api/src/healthcheck.js
+++ api/src/healthcheck.js
@@ -5,17 +5,29 @@ const GIT_TAG_DEFAULT = 'There was no tag associated with this build'
 
 
 const getLatestBlock = async (web3) => {
-  return await web3.eth.getBlockNumber()
+  try {
+    const lastBlockNumber = await web3.eth.getBlockNumber()
+    return {
+      lastBlockNumber,
+      status: 'Running'
+    }
+  } catch (err) {
+    return {
+      lastBlockNumber: 0,
+      status: err.message
+    }
+  }
 }
 
 // GIT_COMMIT_SHA and GIT_TAG are populated when creating the docker image
 const healthcheck = (contractAddress, web3) => {
   return async (ctx, next) => {
-    const latestBlockNumber = await getLatestBlock(web3)
+    const { lastBlockNumber, status } = await getLatestBlock(web3)
     const healthCheckMethod = abHealthcheck({
       custom: {
         storeContractAddress: contractAddress,
-        lastestBlockNumber: latestBlockNumber,
+        lastestBlockNumber: lastBlockNumber,
+        parityStatus: status,
         commit: process.env.GIT_COMMIT_SHA || GIT_COMMIT_SHA_DEFAULT,
         tag: process.env.GIT_TAG || GIT_TAG_DEFAULT
       }
