7f1c68924d1d78655057d6545552ae7b29e1888b
Michael <michael@appliedblockchain.com>
2018-11-20 18:13:16 +0000

    Finished the NotesAdd form

--- react/src/containers/NotesAdd/NotesAdd.js
+++ react/src/containers/NotesAdd/NotesAdd.js
@@ -1,17 +1,16 @@
 import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
-// import crypto from 'crypto'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
 import { Button, FormLabel, Input, IconButton } from '@material-ui/core'
-import { AddBox as AddBoxIcon, IndeterminateCheckBox as IndeterminateCheckBoxIcon  } from '@material-ui/icons'
-// import { API_PUBLIC_KEY } from 'config'
+import { AddBox as AddBoxIcon, IndeterminateCheckBox as IndeterminateCheckBoxIcon } from '@material-ui/icons'
+import { API_PUBLIC_KEY } from 'config'
 import styles from './style.module.css'
 
 const initialFormValues = {
   tag: '',
   text: '',
-  publicKeys: [ '' ]
+  publicKeys: []
 }
 
 const addNoteValidation = Yup.object().shape({
@@ -26,15 +25,39 @@ const displayError = (errors, touched, field) =>
 const displayErrorList = (errors, touched, field, index) =>
   errors[field] && errors[field][index] && touched[field] && touched[field][index]
 
+const hasErrors = errors => {
+  const regularErrors = { ...errors }
+  const arrayErrors = errors.publicKeys ? [ ...errors.publicKeys ] : []
+  delete regularErrors.publicKeys
+  const hasRegularErrors = Object.values(regularErrors).map(v => !!v).includes(true)
+  const hasArrayErrors = Object.values(arrayErrors).map(v => !!v).includes(true)
+
+  return hasRegularErrors || hasArrayErrors
+}
+
+const hasBeenTouched = touched => Object.keys(touched).length > 0
+
 class NotesAdd extends Component {
   onSubmit = (values, hooks) => {
-    console.log('this.onSubmit value: ', values)
-    console.log('this.onSubmit hooks: ', hooks)
+    const { addNote } = this.props
+    const { tag, text, publicKeys } = values
+
+    const uniquePublicKeys = [ ...[
+      ...new Set(publicKeys.map(key => key.trim())), API_PUBLIC_KEY ]
+    ]
+
+    addNote(tag, text, uniquePublicKeys)
+    hooks.resetForm()
   }
 
-  _addPublicKey = (setFieldValue, values) => () => setFieldValue(
+  addPublicKey = (setFieldValue, values) => () => setFieldValue(
     'publicKeys',
     [ ...values.publicKeys, '' ]
+  )
+
+  removePublicKey = (setFieldValue, values, index) => () => setFieldValue(
+    'publicKeys',
+    [ ...values.publicKeys.slice(0, index), ...values.publicKeys.slice(index + 1) ]
   )
 
   renderTag = ({ handleChange, handleBlur, values, errors, touched }) => (
@@ -75,20 +98,29 @@ class NotesAdd extends Component {
       <FormLabel component="h3">Public Keys</FormLabel>
       { values.publicKeys.map((publicKey, index) => (
         <Fragment key={`publicKeys[${index}]`}>
-          <Input
-            name={`publicKeys[${index}]`}
-            type="text"
-            placeholder="Enter the public key"
-            value={publicKey}
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
+          <div className={styles.flex}>
+            <Input
+              className={styles.flex1}
+              name={`publicKeys[${index}]`}
+              type="text"
+              placeholder="Enter the public key"
+              value={publicKey}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <IconButton onClick={this.removePublicKey(setFieldValue, values, index)}>
+              <IndeterminateCheckBoxIcon
+                color="primary"
+                className={styles.checkboxIcon}
+              />
+            </IconButton>
+          </div>
           { displayErrorList(errors, touched, 'publicKeys', index) &&
           <div className={styles.fieldErrors}>{errors.publicKeys[index]}</div> }
         </Fragment>
       )) }
       <div className={styles.addContainer}>
-        <IconButton onClick={this._addPublicKey(setFieldValue, values)}>
+        <IconButton onClick={this.addPublicKey(setFieldValue, values)}>
           <AddBoxIcon color="primary" className={styles.checkboxIcon} />
         </IconButton>
       </div>
@@ -102,6 +134,15 @@ class NotesAdd extends Component {
         { this.renderTag(formProps) }
         { this.renderText(formProps) }
         { this.renderPublicKeys(formProps) }
+        <Button
+          type="submit"
+          variant="outlined"
+          color="primary"
+          className={styles.buttonSubmit}
+          disabled={hasErrors(formProps.errors) || !hasBeenTouched(formProps.touched)}
+        >
+          ADD NOTE
+        </Button>
       </form>
     )
   }
@@ -124,137 +165,3 @@ NotesAdd.propTypes = {
 }
 
 export default (NotesAdd)
-
-// class NotesAdd extends Component {
-  // addPublicKey = () => {
-  //   const { form } = this.props
-  //   const publicKeyIds = form.getFieldValue('publicKeyIds')
-  //   const uuid = crypto.randomBytes(16).toString('hex')
-  //   publicKeyIds[uuid] = uuid
-  //   form.setFieldsValue({
-  //     publicKeyIds
-  //   })
-  // }
-
-//   removePublicKey = uuid => {
-//     const { form } = this.props
-//     const publicKeyIds = form.getFieldValue('publicKeyIds')
-//     delete publicKeyIds[uuid]
-//     form.setFieldsValue({
-//       publicKeyIds
-//     })
-//   }
-
-//   onSubmit = ev => {
-//     ev.preventDefault()
-//     const { addNote, form } = this.props
-
-//     form.validateFields((error, { tag, text, publicKeys }) => {
-//       if (error) {
-//         console.error('form validation error', error)
-//         return
-//       }
-
-//       const uniquePublicKeys = [ ...[
-//         ...new Set(Object.values(publicKeys || {}))
-//       ].map(key => key.trim()), API_PUBLIC_KEY ]
-
-//       addNote(tag, text, uniquePublicKeys)
-
-//       // temporary solution
-//       form.resetFields()
-//     })
-//   }
-
-//   renderPublicKeysFields = () => {
-//     const { getFieldDecorator, getFieldValue, getFieldError } = this.props.form
-//     getFieldDecorator('publicKeyIds', { initialValue: {} })
-
-//     const keys = getFieldValue('publicKeyIds')
-//     return Object.values(keys).map(publicKeyId => {
-//       return (
-        // <div key={publicKeyId} className={styles.flex}>
-        //   <div className={styles.flex1}>
-//             {getFieldDecorator(`publicKeys[${publicKeyId}]`, {
-//               rules: [
-//                 {
-//                   required: true,
-//                   message: 'Please provide a public key of a user'
-//                 }
-//               ],
-//               initialValue: ''
-//             })(<Input className={styles.pubKey} placeholder="Public key of a user to share note with" />)}
-//             <div className={styles.fieldErrors}>
-//               {getFieldError(`publicKeys[${publicKeyId}]`)
-//                 ? getFieldError(`publicKeys[${publicKeyId}]`).join(',')
-//                 : null}
-//             </div>
-//           </div>
-          // <IconButton onClick={() => this.removePublicKey(publicKeyId)}>
-          //   <IndeterminateCheckBoxIcon
-          //     color="primary"
-          //     className={styles.checkboxIcon}
-          //   />
-          // </IconButton>
-//         </div>
-//       )
-//     })
-//   }
-
-//   render() {
-//     const { getFieldDecorator, getFieldError } = this.props.form
-
-//     return (
-//       <form onSubmit={this.onSubmit} className={styles.container}>
-//         <FormLabel component="h3">Note Tag</FormLabel>
-//         {getFieldDecorator('tag', {
-//           rules: [ { required: true, message: 'Please input a note tag!' } ],
-//           initialValue: ''
-//         })(
-//           <Input
-//             label="Note Tag"
-//             placeholder="Note tag, visible for all users"
-//           />
-//         )}
-//         <div className={styles.fieldErrors}>
-//           {getFieldError('tag') ? getFieldError('tag').join(',') : null}
-//         </div>
-//         <FormLabel component="h3">Secret Note</FormLabel>
-//         {getFieldDecorator('text', {
-//           rules: [ { required: true, message: 'Please input some text!' } ],
-//           initialValue: ''
-//         })(
-//           <Input
-//             multiline
-//             rows={2}
-//             rowsMax={8}
-//             placeholder="Leave a secrete note here"
-//             className={styles.field}
-//           />
-//         )}
-//         <div className={styles.fieldErrors}>
-//           {getFieldError('text') ? getFieldError('text').join(',') : null}
-//         </div>
-
-//         <FormLabel component="h3">
-//           Provide public keys of users which you want to share note with
-//         </FormLabel>
-//         {this.renderPublicKeysFields()}
-        // <div className={styles.addContainer}>
-        //   <IconButton onClick={this.addPublicKey}>
-        //     <AddBoxIcon color="primary" className={styles.checkboxIcon} />
-        //   </IconButton>
-        // </div>
-
-//         <Button
-//           type="submit"
-//           variant="outlined"
-//           color="primary"
-//           className={styles.buttonSubmit}
-//         >
-//           ADD NOTE
-//         </Button>
-//       </form>
-//     )
-//   }
-// }
