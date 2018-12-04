68e3566ebd44b6f54bfc5cb97754e71b425023b7
Francesco 'makevoid <makevoid@gmail.com>
2018-12-04 15:05:09 +0000

    move app.js/run.js to async/await, move main error catching to run

--- api/run.js
+++ api/run.js
@@ -1,5 +1,22 @@
 'use strict'
 
 const runApp = require('./src/app')
+const logger = require('./src/logger')
+const config = require('config')
 
-runApp(process.env.CONTRACT_ADDRESS)
+const NODE_ENV = config.get('NODE_ENV')
+const PORT = config.get('PORT')
+
+;(async () => {
+  try {
+    const contractAddress = process.env.CONTRACT_ADDRESS
+    await runApp(contractAddress)
+    logger.debug(`Server listening on ${PORT} in '${NODE_ENV}' mode`)
+    logger.debug(`Contract deployed at ${contractAddress}`)
+    logger.debug(`Docs available at http://localhost:${PORT}/docs`)
+  } catch (err) {
+    logger.error('Error while starting up server')
+    logger.error(err)
+    process.exit(1)
+  }
+})()
--- api/src/app.js
+++ api/src/app.js
@@ -1,25 +1,11 @@
-const assert = require('assert')
 const createServer = require('./server')
-const logger = require('./logger')
 const config = require('config')
 
 const PORT = config.get('PORT')
-const NODE_ENV = config.get('NODE_ENV')
 
-const runApp = contractAddress => {
-  createServer(contractAddress).then(
-    app =>
-      app.listen(PORT, err => {
-        assert(!err, err)
-        console.log(`Contract deployed at ${contractAddress}`)
-        console.log(`Docs available at http://localhost:${PORT}/docs`)
-        logger.debug(`Server listening on ${PORT} in ${NODE_ENV} mode`)
-      }),
-    err => {
-      logger.error('Error while starting up server', err)
-      process.exit(1)
-    }
-  )
+const runApp = async contractAddress => {
+  const app = await createServer(contractAddress)
+  await app.listen(PORT)
 }
 
 module.exports = runApp
