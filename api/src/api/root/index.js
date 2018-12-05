b8d6af6bb20b627dd69af21470057d8e1149d503
Sabin Raj Dangol <sawbean.raz@googlemail.com>
2018-12-05 19:06:09 +0000

    Update root api to call hello World

--- api/src/api/root/index.js
+++ api/src/api/root/index.js
@@ -1,5 +1,42 @@
 'use strict'
+const Joi = require('koa-joi-router').Joi
 
-module.exports = {
-  routes: require('./routes')
+const handler = async (ctx) => {
+  const { HelloWorldContract } = ctx.contracts
+  const helloWorldString = await HelloWorldContract.methods.getHelloWorld().call()
+
+  ctx.ok({ message: helloWorldString })
 }
+
+module.exports = [
+  {
+    method: 'get',
+    path: '/',
+    validate: {
+      output: {
+        200: {
+          body: {
+            message: Joi.string()
+          }
+        }
+      }
+    },
+    handler
+  },
+  {
+    method: 'post',
+    path: '/',
+    validate: {
+      type: 'json',
+      body: {
+        message: Joi.string().required()
+      }
+    },
+    handler: async (ctx) => {
+      const { message } = ctx.request.body
+      const { HelloWorldContract } = ctx.contracts
+      await HelloWorldContract.methods.updateHelloWorld(message).send()
+      ctx.ok(message)
+    }
+  }
+]
