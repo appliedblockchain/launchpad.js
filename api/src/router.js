eff10b40cf8c4c3ee1e47ee3171c68fe1f05f6b0
Sabin Raj Dangol <sawbean.raz@googlemail.com>
2018-12-06 16:00:56 +0000

    Remove all secret notes related files

--- api/src/router.js
+++ api/src/router.js
@@ -1,31 +1,20 @@
 'use strict'
 
 const koaRouter = require('koa-joi-router')
-const { ipfs, parityProxy } = require('@appliedblockchain/mantle-api')
 
 const configureDocs = require('./api/docs-config')
-const { routes: rootRoutes } = require('./api/root') // or const rootRoutes = require('./api/root/routes')
-const { routes: noteRoutes } = require('./api/notes') // or const noteRoutes = require('./api/notes/routes')
-const { routes: errorRoutes } = require('./api/Error')
+const rootRoute = require('./api/root')
 const { API_PREFIX } = require('./constants')
 
 const router = koaRouter()
 
-router.route(noteRoutes)
-router.route(errorRoutes)
-router.use('', ipfs.createRouter().prefix('/ipfs').middleware())
-router.use('', parityProxy.createRouter().middleware())
-router.route(rootRoutes)
+router.route(rootRoute)
 router.prefix(API_PREFIX)
 
 module.exports = {
   configureDocs,
   middleware: router.middleware(),
   routes: {
-    error: errorRoutes,
-    notes: noteRoutes,
-    default: rootRoutes,
-    ipfs: ipfs.routes,
-    transactions: parityProxy.routes
+    default: rootRoute
   }
 }
