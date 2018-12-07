7bbe713d96535ed5009e171c1ae9524839218bb2
Sabin Raj Dangol <sawbean.raz@googlemail.com>
2018-12-07 15:08:41 +0000

    Remove react folder

--- react/src/store/auth/saga/index.js
+++
@@ -1,8 +0,0 @@
-import { all } from 'redux-saga/effects'
-import watchGenerateMnemonic from './generateMnemonic'
-import watchLoadMnemonic from './loadMnemonic'
-import watchLogout from './logout'
-
-export default function* rootSaga() {
-  yield all([ watchGenerateMnemonic(), watchLoadMnemonic(), watchLogout() ])
-}
--- react/src/utils/fullName.js
+++
@@ -1,5 +0,0 @@
-import { APP_PREFIX } from 'constants.js'
-
-const fullName = (state, action) => `${APP_PREFIX}/${state}/${action}`
-
-export default fullName
