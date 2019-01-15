5925c85880f1af195703e138fee31e951350130a
makevoid <makevoid@gmail.com>
2019-01-15 01:53:19 +0000

    fix parity's entrypoint - switch from consul to redis discovery

---
+++ stack/docker/entrypoint-parity.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+function disco() {
+  redis-cli -h discovery --raw $*
+}
+
+[ "$(disco ping)" == "PONG" ] || { echo "discovery redis not up, exiting..." && exit; }
+
+set -ex
+
+if [ "$1" = "restore" ]; then
+  node /parity/download.js "$2"
+  tar -xzvf "$2" -C /
+
+  exit
+fi
+
+sleep 1
+
+for i in 1 2 3; do
+  ENODE=$(disco get "enodes:parity$i")
+  echo "$ENODE" >> reserved_peers
+done
+
+# NOTE: uncomment to activate backups
+# node /parity/cron-job.js
+
+/parity/parity --chain /parity/spec.json --config /parity/authority.toml -d /parity/data
