007cde32181ac166ea33009b679a6d5e0ad07ea8
makevoid <makevoid@gmail.com>
2019-03-07 05:11:34 +0000

    update deploy.js to generate contractAddresses.json and contractsABI.json

--- contracts/bin/deploy.js
+++ contracts/bin/deploy.js
@@ -1,72 +1,118 @@
+/* eslint no-useless-constructor: "off" */
 'use strict'
 
-const fs = require('fs')
+const { readdirSync, readFileSync, writeFileSync } = require('fs')
 const { join } = require('path')
 const Web3 = require('web3')
 
 const contractsDirectory = join(__dirname, '../build/contracts')
-const contractsFilenames = fs.readdirSync(contractsDirectory).filter(f => /\.json$/.test(f))
+const contractsFilenames = readdirSync(contractsDirectory).filter(f => /\.json$/.test(f))
 
 const config = require('config')
 const PROVIDER = config.provider || process.env.PROVIDER
 
-if (contractsFilenames.length === 0) {
-  throw new Error('Contracts not found, you should run \'npm run compile\'')
-}
+class ContractNotFoundError extends Error {}
 
-const contracts = {}
-contractsFilenames.forEach(file => {
-  const fileString = fs.readFileSync(join(contractsDirectory, file))
-
+const addContract = (contracts, contractFilePath) => {
+  console.log(contracts, contractFilePath)
+  const fileString = readFileSync(join(contractsDirectory, contractFilePath))
   try {
     const contract = JSON.parse(fileString)
-    contracts[contract.contractName] = contract
+    const name = contract.contractName
+    console.log(`loaded contract: ${name}`)
+    contracts[name] = contract
+    return contracts
   } catch (err) {
-    console.error(`Can not parse ${file}`)
+    console.error(`Can not parse contract JSON info - path: ${contractFilePath}`)
+    console.log('exiting...')
     process.exit(1)
   }
-});
+}
 
-(async () => {
-  const web3 = new Web3(new Web3.providers.HttpProvider(PROVIDER))
+const buildSendParams = (defaultAddress) => {
+  const from = process.env.DEFAULT_ADDRESS || process.env.ETH_ADDRESS || process.env.FROM || defaultAddress
 
-  try {
-    const coinbase = await web3.eth.getCoinbase()
-    const from = process.env.FROM || coinbase // default from for parity-solo
-
-    const sendParams = {
-      from,
-      gas: 50000000
-    }
-
-    const contractNames = Object.keys(contracts)
-
-    const deployedContracts = {}
-    for (let i = 0; i < contractNames.length; i++) {
-      const contractName = contractNames[i]
-      console.log(`Deploying contract: ${contractName}....\n`)
-      const { abi, bytecode } = contracts[contractName]
-
-
-      let contract = new web3.eth.Contract(abi, { from, data: bytecode })
-      contract = await contract.deploy().send(sendParams)
-
-      deployedContracts[contractName] = { abi, address: contract.options.address }
-    }
-
-    const contractsJSON = JSON.stringify(deployedContracts, null, 2)
-
-    console.log('Writing contract contract information....\n')
-
-    const path = join(__dirname, '../build/combined.json')
-    fs.writeFileSync(path, contractsJSON)
-    console.log(`Contract information saved at ${path}`)
-
-  } catch (err) {
-    if (err.message === 'Invalid JSON RPC response: ""') {
-      console.error('Error: Unable to connect to network, is parity running?')
-    } else {
-      console.error(err)
-    }
+  return {
+    from,
+    gas: 50000000
   }
-})()
+}
+
+const deployContracts = async ({ ctrNames, contracts, defaultAddress, eth }) => {
+  const contractsABI = {}
+  for (let i = 0; i < ctrNames.length; i++) {
+    const contractName = ctrNames[i]
+    console.log(`Deploying contract: ${contractName}....\n`)
+
+    if (contractName === 'Migrations') {
+      continue
+    }
+
+    const { abi, bytecode } = contracts[contractName]
+    const sendParams = buildSendParams(defaultAddress)
+
+    let contract = new eth.Contract(abi, { from: sendParams.from, data: bytecode })
+    contract = await contract.deploy().send(sendParams)
+
+    contractsABI[contractName] = { abi, address: contract.options.address }
+  }
+  return contractsABI
+}
+
+const deploy = () => {
+  const contracts = contractsFilenames.reduce((contracts, file) => {
+    return addContract(contracts, file)
+  }, {})
+
+  ;(async () => {
+    const provider = new Web3.providers.HttpProvider(PROVIDER)
+    const web3 = new Web3(provider)
+    const { eth } = web3
+
+    try {
+      // load default address (ethereum account)
+      const defaultAddress = await web3.eth.getCoinbase()
+      console.log(`oaded address: ${defaultAddress} (coinbase)`)
+      // load a specific address:
+      //    const address = await web3.eth.getAccounts()[0]
+      //    console.log(`loaded address: ${address}`)
+
+      const ctrNames = Object.keys(contracts)
+      console.log(`Contracts deployment - contracts: ${ctrNames.join(', ')}`)
+
+      const contractsABI = await deployContracts({ ctrNames, contracts, defaultAddress, eth })
+      console.log('Contracts deployed!\n')
+
+      const contractsAbiJSON = JSON.stringify(contractsABI, null, 2)
+      const path = join(__dirname, '../build/contractsABI.json')
+      writeFileSync(path, contractsAbiJSON)
+      console.log(`Contract info saved: ${path}`)
+
+      const contractAddresses = contractsAbiJSON.map(ctrInfo => (ctrInfo.address))
+      const pathSh = join(__dirname, '../config/contractAddresses.json')
+      writeFileSync(pathSh, contractAddresses)
+      console.log(`Contract addresses sh saved: ${pathSh}`)
+    } catch (err) {
+      if (err.message === 'Invalid JSON RPC response: ""') {
+        console.error('Error: Unable to connect to network, is parity running?')
+      } else {
+        console.error(err)
+      }
+    }
+  })()
+}
+
+const checkContracts = () => {
+  if (contractsFilenames.length === 0) {
+    throw new ContractNotFoundError('Contracts not found, you should run "npm run compile"')
+  }
+}
+
+const main = () => {
+  checkContracts()
+
+  console.log('deployment started...')
+  deploy()
+}
+
+main()
