69f24b4a07cf64026e325a7232039d26515c78f4
makevoid <makevoid@gmail.com>
2019-11-20 15:56:45 +0000

    removed redundant enodes file write

--- stack/entrypoint-parity.sh
+++ stack/entrypoint-parity.sh
@@ -10,12 +10,4 @@ set -ex
 
 sleep 3
 
-rm -f reserved_peers
-
-for i in 1 2 3; do
-  ENODE=$(disco get "enodes:parity$i")
-  echo "$ENODE" >> reserved_peers
-done
-
-
 parity --chain /parity/spec.json --config /parity/authority.toml -d /parity/data
